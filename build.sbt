ce31a09fe0dbfd44413bf44db8814aa335d24aab
mariadroman <mariadroman@gmail.com>
2015-11-19 14:59:09 +0100

    Remove println Println is not required for feeder functionality

--- build.sbt
+++ build.sbt
@@ -5,3 +5,10 @@ lazy val root = (project in file(".")).
     scalaVersion := "2.11.4",
     libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.8.2.2"
   )
+
+val meta = """META.INF(.)*""".r
+assemblyMergeStrategy in assembly := {
+  case meta(_) => MergeStrategy.discard
+  case "UnusedStubClass.class" => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
--- src/main/scala/feeder/KafkaFeeder.scala
+++ src/main/scala/feeder/KafkaFeeder.scala
@@ -3,35 +3,51 @@ package feeder
 import java.util.HashMap
 import java.io.File
 import scala.io.Source
-
 import org.apache.kafka.clients.producer.{ProducerConfig, KafkaProducer, ProducerRecord}
 
+/**
+ * Send messages to a particular Kafka topic
+ */
 object KafkaFeeder {
 
+  /**
+   * Send messagess to a particular Kafka topic
+   * @param args: expected 4 args:
+   *            1) brokerList: List of Kafka brokers separated by commas.
+   *                           e.g.: "localhost:9092,localhost:9093"
+   *            2) topic: Kafka topic whereto send the messages.
+   *            3) githubDataDirectory: directory containing githubArchive data files.
+   *            4) invervalBetweenFiles: time in milliseconds used as interval between two files processing.
+   */
   def main(args: Array[String]) {
     if (args.length < 4) {
       System.err.println("Usage: KafkaFeeder <brokerList> <topic> <githubDataDirectory> <intervalBetweenFiles>")
       System.exit(1)
     }
+
     val Array(brokers, topic, githubDataDirectory, intervalBetweenFiles) = args
     val producer = constructProducer(brokers)
 
     // Loop through json files in directory
     new File(githubDataDirectory).listFiles().toSeq.foreach{ file =>
-      println(s"Processing file ${file.getName}")
       // Process them line by line. Each line corresponds to a single event
       for (line <- Source.fromFile(file, "utf-8").getLines()) {
-        println(s"Pushing event to topic '$topic'")
         // Construct record and feed it into kafka.
         val event = new ProducerRecord[String, String](topic, null, line)
         producer.send(event)
       }
-
       println(s"Processed file ${file.getName}, sleeping for $intervalBetweenFiles milliseconds")
       Thread.sleep(intervalBetweenFiles.toLong)
     }
+
+    producer.close()
   }
 
+  /**
+   * Create a producer
+   * @param brokers List of Kafka brokers
+   * @return Producer
+   */
   def constructProducer(brokers: String) : KafkaProducer[String,String] = {
     val props = new HashMap[String, Object]()
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
